bf5fcd92d3be45456f25d0bf75ab5b3b945407b3
Helen Oliveira <sousahelen816@gmail.com>
2022-08-30 18:07:41 -0300

    component home

--- supermarketcart/src/app/app.module.ts
+++ supermarketcart/src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NavigationComponent,
+    HomeComponent
+  ],  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
+ 
+ 
   providers: [],
   bootstrap: [AppComponent]
 })
